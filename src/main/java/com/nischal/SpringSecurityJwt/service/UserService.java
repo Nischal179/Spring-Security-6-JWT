b27679fcf5e79206c3693e7bfaddf2867b80a8e1
Nischal876 <nischalaryal11@gmail.com>
2024-08-18 15:30:49 +0545

    Create a UserController and UserService class to allow registeration of new users

---
+++ src/main/java/com/nischal/SpringSecurityJwt/service/UserService.java
@@ -0,0 +1,12 @@
+package com.nischal.SpringSecurityJwt.service;
+
+import com.nischal.SpringSecurityJwt.model.Users;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public Users register(Users users) {
+
+    }
+}
