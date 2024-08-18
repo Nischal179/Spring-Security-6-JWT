c7ee20de8ec33974be9eecabde47ca359b0ce18a
Nischal876 <nischalaryal11@gmail.com>
2024-08-18 15:51:51 +0545

    Added password encoding using BCrypt - Encoding users password while registering new users - Modifying SecurityFilterChain to convert encode password while checking for authentication

--- src/main/java/com/nischal/SpringSecurityJwt/controller/UserController.java
+++ src/main/java/com/nischal/SpringSecurityJwt/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.nischal.SpringSecurityJwt.controller;
 
 import com.nischal.SpringSecurityJwt.model.Users;
+import com.nischal.SpringSecurityJwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @PostMapping("/register")
     public Users register(@RequestBody Users users) {
-        return users;
+        return service.register(users);
     }
 }
--- src/main/java/com/nischal/SpringSecurityJwt/service/UserService.java
+++ src/main/java/com/nischal/SpringSecurityJwt/service/UserService.java
@@ -1,12 +1,22 @@
 package com.nischal.SpringSecurityJwt.service;
 
 import com.nischal.SpringSecurityJwt.model.Users;
+import com.nischal.SpringSecurityJwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users users) {
+        users.setPassword(encoder.encode(users.getPassword()));
+        return (userRepository.save(users));
 
     }
 }
