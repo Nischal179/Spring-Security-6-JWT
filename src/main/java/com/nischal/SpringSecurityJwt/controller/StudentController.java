349a057b2f652ebf96e4ebca80e6ba5acecda9f5
Nischal876 <nischalaryal11@gmail.com>
2024-08-18 11:16:58 +0545

    Add SecurityConfig with CSRF disabled and stateless sessions

--- src/main/java/com/nischal/SpringSecurityJwt/controller/StudentController.java
+++ src/main/java/com/nischal/SpringSecurityJwt/controller/StudentController.java
@@ -1,4 +1,17 @@
 package com.nischal.SpringSecurityJwt.controller;
 
+
+import com.nischal.SpringSecurityJwt.dto.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+//    @GetMapping("/students")
+//    public List<Student> getStudents() {
+//
+//    }
 }
