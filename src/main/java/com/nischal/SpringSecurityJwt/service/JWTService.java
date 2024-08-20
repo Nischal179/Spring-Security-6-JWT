d38ced70fe63f725db03bf2f9d8e5663dcff5f08
Nischal876 <nischalaryal11@gmail.com>
2024-08-20 11:23:43 +0545

    feat: Generate and encode secret key in JWTService
    - Added JWTService constructor to generate a secret key using HmacSHA256 and encode it in Base64.
    - Implemented getKey method to decode the Base64 secret key and create an HMAC SHA key.
    
    These changes ensure secure generation and handling of the JWT signing key.

--- src/main/java/com/nischal/SpringSecurityJwt/service/JWTService.java
+++ src/main/java/com/nischal/SpringSecurityJwt/service/JWTService.java
@@ -5,7 +5,11 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +17,20 @@ import java.util.Map;
 @Service
 public class JWTService {
 
-    private String secretKey = "g2_72M9.fgh12";
+    private String secretKey = "";
+
+    public JWTService() {
+        try
+        {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGenerator.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        }
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     public String generateToken(String username) {
 
@@ -24,9 +41,16 @@ public class JWTService {
                 .add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() * 60 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
                 .and()
                 .signWith(getKey())
                 .compact();
+    }
+
+    // Method to sign the JWT for data signature
+    private Key getKey() {
+        // Using base64 decoder to convert your string to byte
+        byte[] keyByte = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyByte);
     }
 }
